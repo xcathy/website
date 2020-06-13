11f8b1058ac4b0501bb09e4fa0be85398d711115
Cathy Xie <cathyxie97@gmail.com>
2020-06-13 10:42:16 -0700

    2020-06-13

--- js/main.js
+++ js/main.js
@@ -13,6 +13,8 @@ var radius = canvas.height / 2;
 ctx.translate(radius, radius);
 radius = radius * 0.90
 drawFace(ctx, radius);
+drawNumbers(ctx, radius);
+drawTime(ctx, radius);
 
 function drawFace(ctx, radius) {
     var grad;
@@ -21,7 +23,7 @@ function drawFace(ctx, radius) {
     ctx.arc(0, 0, radius, 0, 2 * Math.PI);
     ctx.fillStyle = '#f0c88c';
     ctx.fill();
-    
+
     /* clock face color */
     grad = ctx.createRadialGradient(0, 0 ,radius * 0.95, 0, 0, radius * 1.05);
     grad.addColorStop(0, '#f0c88c');
@@ -45,6 +47,54 @@ function drawFace(ctx, radius) {
     grad.addColorStop(0.75, '#c1905e');
     grad.addColorStop(1, '#f0c88c');
     ctx.strokeStyle = grad;
-    ctx.lineWidth = radius*0.05;
+    ctx.lineWidth = radius*0.08;
     ctx.stroke();
 }
+
+function drawNumbers(ctx, radius) {
+    var ang;
+    var num;
+    ctx.font = radius * 0.2 + "px arial";
+    ctx.textBaseline = "middle";
+    ctx.textAlign = "center";
+    
+    for(num = 1; num < 13; num++){
+        ang = num * Math.PI / 6;
+        ctx.rotate(ang);
+        ctx.translate(0, -radius * 0.85);
+        ctx.rotate(-ang);
+        ctx.fillText(num.toString(), 0, 0);
+        ctx.rotate(ang);
+        ctx.translate(0, radius * 0.85);
+        ctx.rotate(-ang);
+    }
+}
+
+function drawTime(ctx, radius){
+    var now = new Date();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
+    //hour
+    hour = hour%12;
+    hour = (hour*Math.PI/6)+(minute*Math.PI/(6*60))+(second*Math.PI/(360*60));
+    drawHand(ctx, hour, radius*0.5, radius*0.07);
+    //minute
+    minute = (minute*Math.PI/30)+(second*Math.PI/(30*60));
+    drawHand(ctx, minute, radius*0.8, radius*0.07);
+    // second
+    second = (second*Math.PI/30);
+    drawHand(ctx, second, radius*0.78, radius*0.02);
+}
+
+  function drawHand(ctx, pos, length, width) {
+    ctx.beginPath();
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = width;
+   
+    ctx.moveTo(0,0);
+    ctx.rotate(pos);
+    ctx.lineTo(0, -length);
+    ctx.stroke();
+    ctx.rotate(-pos);
+  }
