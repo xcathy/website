52a462a78723565140d77b2d8f409129a3b4005f
lxie <cathyxie97@gmail.com>
2024-10-26 23:58:58 -0700

    updated home page formatting

--- blog/app/(tabs)/index.tsx
+++ blog/app/(tabs)/index.tsx
@@ -10,25 +10,37 @@ import { ScrollView } from 'react-native-gesture-handler';
 export default function HomeScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      <Image
-        style={styles.banner}
-        source={Images.jellyfish1}
-        placeholder={ Colors.blurhash }
-        contentFit="cover"
-        transition={1000}
-      />
       <ScrollView
         style={styles.scrollView}
         showsVerticalScrollIndicator={ false }
       >
-          
-        <ThemedView style={styles.titleContainer}>
-          <ThemedText type="title">Welcome to my blog!</ThemedText>
-        </ThemedView>
-
-        <ThemedView style={styles.contentContainer}>
-        </ThemedView>
+        <Image
+          style={styles.banner}
+          source={Images.jellyfish1}
+          placeholder={ Colors.blurhash }
+          contentFit="cover"
+          transition={1000}
+        />
       </ScrollView>
+      <ThemedView style={styles.titleContainer} >
+        <ThemedText
+          type="title"
+          lightColor="white"
+          darkColor="white"
+        >
+          Welcome to my blog!
+        </ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.contentContainer}>
+        <ThemedText
+          type="default"
+          lightColor="#EEEEEE"
+          darkColor="white"
+        >
+          New updates incoming...
+        </ThemedText>
+      </ThemedView>
+      
     </SafeAreaView>
   );
 }
@@ -47,24 +59,25 @@ const styles = StyleSheet.create({
   },
   banner: {
     width: Dimensions.get('window').width,
-    height: 0.4 * Dimensions.get('window').height,
-    shadowColor: '#001a33',
-    shadowRadius: 15,
-    shadowOpacity: 0.6,
+    height: Dimensions.get('window').height,
     alignItems: 'center',
   },
   titleContainer: {
     flexDirection: 'row',
-    alignItems: 'center',
-    color: '#FFFFFF',
-    backgroundColor: '#BDD5E7',
-    margin: 30,
+    position: 'absolute',
+    alignSelf: 'center',
+    marginVertical: 0.4 * Dimensions.get('window').height,
+    backgroundColor: 'none',
+    textShadowColor: '#001a3380',
+    textShadowRadius: 15,
   },
   contentContainer: {
     flexDirection: 'row',
-    alignItems: 'center',
-    color: '#FFFFFF',
-    backgroundColor: '#BDD5E7',
-    marginHorizontal: 30,
+    position: 'absolute',
+    alignSelf: 'center',
+    marginTop: 0.3 * Dimensions.get('window').height,
+    backgroundColor: 'none',
+    textShadowColor: '#001a3380',
+    textShadowRadius: 15,
   },
 });
