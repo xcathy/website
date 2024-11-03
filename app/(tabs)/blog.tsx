ecd41d99d8e46b0df1e1c0578441308c5d747c0d
lxie <cathyxie97@gmail.com>
2024-11-03 01:30:39 -0800

    updated blog to gridview

--- app/(tabs)/blog.tsx
+++ app/(tabs)/blog.tsx
@@ -1,57 +1,72 @@
 import { StyleSheet} from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { Images } from '@/constants/Images';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import { Dimensions } from 'react-native';
 import { BlogPost } from '@/components/BlogPost';
+import GridLayout from 'react-grid-layout';
 
 export default function TabTwoScreen() {
   return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView
-        style={styles.scrollView}
-        showsVerticalScrollIndicator={ false }
+    <ScrollView
+      style={styles().scrollView}
+      showsVerticalScrollIndicator={ false }
+    >
+      <GridLayout
+        className="layout"
+        cols={12}
+        rowHeight={30}
+        width={1200}
       >
         {
           BlogPost(
-            Images.flower9,
-            styles.image,
-            "Butchard's Garden Visit",
-            undefined,
-            "Flower pictures from the trip to Butchard's Garden, 2024"
+            "flower9",                                                  //ID, also serve as the key
+            Images.flower9,                                             //Image source
+            styles({ width: 0.45, height: 0.8 }).image,                 //Image style, width and height specifies how much it will be multiplied with respect to the window width and height
+            { x: 0, y: 0, w: 1, h: 2, static: true },                   //Grid layout information, specifies placement of images
+            "Butchard's Garden Visit",                                  //Title
+            undefined,                                                  //Subtitle
+            "Flower pictures from the trip to Butchard's Garden, 2024", //Post content
           )
         }
         {
           BlogPost(
+            "flower5",
             Images.flower5,
-            styles.image,
+            styles({ width: 0.48, height: 0.5 }).image,
+            { x: 6, y: 0, w: 1, h: 2, static: true},
             "Butchard's Garden Visit",
             undefined,
-            "Flower pictures from the trip to Butchard's Garden, 2024"
+            "Flower pictures from the trip to Butchard's Garden, 2024",
           )
         }
         {
           BlogPost(
+            "flower7",
             Images.flower7,
-            styles.image,
+            styles({ width: 0.48, height: 0.8 }).image,
+            { x: 6, y: 7.9, w: 1, h: 2, static: true},
             "Butchard's Garden Visit",
             undefined,
-            "Flower pictures from the trip to Butchard's Garden, 2024"
+            "Flower pictures from the trip to Butchard's Garden, 2024",
           )
         }
         {
           BlogPost(
+            "fountain",
             Images.fountain,
-            styles.image,
+            styles({ width: 0.45, height: 0.8 }).image,
+            { x: 0, y: 12.3, w: 1, h: 2, static: true},
             "Fountain",
             undefined,
-            "The picture of a fountain from the trip to Butchard's Garden, 2024"
+            "The picture of a fountain from the trip to Butchard's Garden, 2024",
           )
         }
         {
           BlogPost(
+            "flower6",
             Images.flower6,
-            styles.image,
+            styles({ width: 0.45, height: 0.8 }).image,
+            { x: 0, y: 24.6, w: 1, h: 2, static: true},
             "Butchard's Garden Visit",
             undefined,
             "Flower pictures from the trip to Butchard's Garden, 2024"
@@ -59,8 +74,21 @@ export default function TabTwoScreen() {
         }
         {
           BlogPost(
+            "frog1",
+            Images.frog1,
+            styles({ width: 0.48, height: 1.1 }).image,
+            { x: 6, y: 20.1, w: 1, h: 2, static: true},
+            "Frogs from Vancouver Aquarium",
+            undefined,
+            "Frog pictures from the trip to Vancouver Aquarium, 2024"
+          )
+        }
+        {
+          BlogPost(
+            "seal",
             Images.seal,
-            styles.image,
+            styles({ width: 0.95, height: 1.5 }).image,
+            { x: 0, y: 36.8, w: 1, h: 2, static: true},
             "Seal :)",
             undefined,
             "Seal from Vancouver Aquarium, I love the smile!\n^U^\n\t\t\t\t\t\t\t\t2024"
@@ -68,53 +96,10 @@ export default function TabTwoScreen() {
         }
         {
           BlogPost(
-            Images.frog1,
-            styles.image,
-            "Frogs from Vancouver Aquarium",
-            undefined,
-            "Frog pictures from the trip to Vancouver Aquarium, 2024"
-          )
-        }
-        {
-          BlogPost(
-            Images.frog2,
-            styles.image,
-            "Frogs from Vancouver Aquarium",
-            undefined,
-            "Frog pictures from the trip to Vancouver Aquarium, 2024"
-          )
-        }
-        {
-          BlogPost(
-            Images.fish1,
-            styles.image,
-            "Fish from Vancouver Aquarium",
-            undefined,
-            "Fish pictures from the trip to Vancouver Aquarium, 2024"
-          )
-        }
-        {
-          BlogPost(
-            Images.fish2,
-            styles.image,
-            "Fish from Vancouver Aquarium",
-            undefined,
-            "Fish pictures from the trip to Vancouver Aquarium, 2024"
-          )
-        }
-        {
-          BlogPost(
-            Images.fish3,
-            styles.image,
-            "Fish from Vancouver Aquarium",
-            undefined,
-            "Fish pictures from the trip to Vancouver Aquarium, 2024"
-          )
-        }
-        {
-          BlogPost(
+            "jellyfish1",
             Images.jellyfish1,
-            styles.image,
+            styles({ width: 0.6, height: 1 }).image,
+            { x: 0, y: 59.4, w: 1, h: 2, static: true},
             "Jellyfish",
             undefined,
             "The jellyfish from Vancouver Aquarium.\nThey look like tiny stars floating in space.\n\t\t\t\t\t\t\t2024"
@@ -122,8 +107,54 @@ export default function TabTwoScreen() {
         }
         {
           BlogPost(
+            "flower4",
+            Images.flower4,
+            styles({ width: 0.27, height: 0.2 }).image,
+            { x: 4.4, y: 74.7, w: 1, h: 2, static: true},
+            "Flowers",
+            undefined,
+            "Flower pictures from Butchard's Garden, 2024"
+          )
+        }
+        {
+          BlogPost(
+            "fish1",
+            Images.fish1,
+            styles({ width: 0.33, height: 0.5 }).image,
+            { x: 8, y: 59.4, w: 1, h: 2, static: true},
+            "Fish from Vancouver Aquarium",
+            undefined,
+            "Fish pictures from the trip to Vancouver Aquarium, 2024"
+          )
+        }
+        {
+          BlogPost(
+            "fish2",
+            Images.fish2,
+            styles({ width: 0.33, height: 0.7 }).image,
+            { x: 8, y: 67.2, w: 1, h: 2, static: true},
+            "Fish from Vancouver Aquarium",
+            undefined,
+            "Fish pictures from the trip to Vancouver Aquarium, 2024"
+          )
+        }
+        {
+          BlogPost(
+            "fish3",
+            Images.fish3,
+            styles({ width: 0.33, height: 0.7 }).image,
+            { x: 0, y: 74.7, w: 1, h: 2, static: true},
+            "Fish from Vancouver Aquarium",
+            undefined,
+            "Fish pictures from the trip to Vancouver Aquarium, 2024"
+          )
+        }
+        {
+          BlogPost(
+            "jellyfish2",
             Images.jellyfish2,
-            styles.image,
+            styles({ width: 0.61, height: 1.3 }).image,
+            { x: 4.4, y: 77.9, w: 1, h: 2, static: true},
             "Jellyfish",
             undefined,
             "The jellyfish from Vancouver Aquarium,\nswimming carelessly in circles.\n\t\t\t\t\t\t2024"
@@ -131,8 +162,10 @@ export default function TabTwoScreen() {
         }
         {
           BlogPost(
+            "flower2",
             Images.flower2,
-            styles.image,
+            styles({ width: 0.33, height: 1.3 }).image,
+            { x: 0, y: 85.5, w: 1, h: 2, static: true},
             "Flowers",
             undefined,
             "Flower pictures from Butchard's Garden, 2024"
@@ -140,8 +173,10 @@ export default function TabTwoScreen() {
         }
         {
           BlogPost(
+            "flower3",
             Images.flower3,
-            styles.image,
+            styles({ width: 0.61, height: 0.49 }).image,
+            { x: 4.4, y: 97.5, w: 1, h: 2, static: true},
             "Flowers",
             undefined,
             "Flower pictures from Butchard's Garden, 2024"
@@ -149,26 +184,21 @@ export default function TabTwoScreen() {
         }
         {
           BlogPost(
-            Images.flower4,
-            styles.image,
-            "Flowers",
+            "frog2",
+            Images.frog2,
+            styles({ width: 0.95, height: 1.5 }).image,
+            { x: 0, y: 105.2, w: 1, h: 2, static: true},
+            "Frogs from Vancouver Aquarium",
             undefined,
-            "Flower pictures from Butchard's Garden, 2024"
+            "Frog pictures from the trip to Vancouver Aquarium, 2024"
           )
         }
         {
           BlogPost(
-            Images.flower5,
-            styles.image,
-            "Flowers",
-            undefined,
-            "Flower pictures from Butchard's Garden, 2024"
-          )
-        }
-        {
-          BlogPost(
+            "flower10",
             Images.flower10,
-            styles.image,
+            styles({ width: 0.4, height: 1 }).image,
+            { x: 0, y: 127.9, w: 1, h: 2, static: true},
             "Flowers",
             undefined,
             "Flower pictures from Butchard's Garden, 2024"
@@ -176,8 +206,10 @@ export default function TabTwoScreen() {
         }
         {
           BlogPost(
+            "flower11",
             Images.flower11,
-            styles.image,
+            styles({ width: 0.54, height: 0.7 }).image,
+            { x: 5.3, y: 127.9, w: 1, h: 2, static: true},
             "Flowers",
             undefined,
             "Flower pictures from Butchard's Garden, 2024"
@@ -185,8 +217,10 @@ export default function TabTwoScreen() {
         }
         {
           BlogPost(
+            "flower12",
             Images.flower12,
-            styles.image,
+            styles({ width: 0.54, height: 1 }).image,
+            { x: 5.3, y: 138.7, w: 1, h: 2, static: true},
             "Waterfall",
             undefined,
             "This is a waterfall display in Butchard's Garden. 2024"
@@ -194,8 +228,10 @@ export default function TabTwoScreen() {
         }
         {
           BlogPost(
+            "flower13",
             Images.flower13,
-            styles.image,
+            styles({ width: 0.4, height: 1 }).image,
+            { x: 0, y: 143.1, w: 1, h: 2, static: true},
             "Flowers",
             undefined,
             "Flower pictures from Butchard's Garden, 2024"
@@ -203,8 +239,10 @@ export default function TabTwoScreen() {
         }
         {
           BlogPost(
+            "flower14",
             Images.flower14,
-            styles.image,
+            styles({ width: 0.54, height: 0.5 }).image,
+            { x: 5.3, y: 153.9, w: 1, h: 2, static: true},
             "Flowers",
             undefined,
             "Flower pictures from Butchard's Garden, 2024"
@@ -212,8 +250,10 @@ export default function TabTwoScreen() {
         }
         {
           BlogPost(
+            "flower15",
             Images.flower15,
-            styles.image,
+            styles({ width: 0.4, height: 1.24 }).image,
+            { x: 0, y: 158.2, w: 1, h: 2, static: true},
             "Flowers",
             undefined,
             "Flower pictures from Butchard's Garden, 2024"
@@ -221,20 +261,21 @@ export default function TabTwoScreen() {
         }
         {
           BlogPost(
+            "flower16",
             Images.flower16,
-            styles.image,
+            styles({ width: 0.54, height: 1 }).image,
+            { x: 5.3, y: 161.7, w: 1, h: 2, static: true},
             "Plant Dog",
             undefined,
             "The plant dog from a display window in Butchard's Garden.\nI left my signature on the signature book near it :D\n\t\t\t\t\t\t\t\t\t\t2024"
           )
         }
-      </ScrollView>
-    </SafeAreaView>
-
+      </GridLayout>
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
+const styles : any = (props: any) => StyleSheet.create({
   scrollView: {
     marginVertical: 0,
     marginHorizontal: 0,
@@ -247,10 +288,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   image: {
-    width: 0.9 * Dimensions.get('window').width,
-    height: 0.8 * Dimensions.get('window').height,
+    width: ( props?.width || 1.0 ) * Dimensions.get('window').width,
+    height: ( props?.height || 1.0 ) * Dimensions.get('window').height,
     margin: 30,
-    borderRadius: 10,
+    borderRadius: 5,
     shadowColor: '#001a33',
     shadowRadius: 15,
     shadowOpacity: 0.6,
--- components/BlogPost.tsx
+++ components/BlogPost.tsx
@@ -5,7 +5,7 @@ import { ThemedText } from "./ThemedText";
 import { ThemedView } from "./ThemedView";
 import { useReducer } from "react";
 
-export function BlogPost( image: string, style?: StyleProp<ImageStyle>, title?: string, subtitle?: string, content?: string, id?: string) : React.JSX.Element {
+export function BlogPost(id: string, image: string, style?: StyleProp<ImageStyle>, layout?: Object, title?: string, subtitle?: string, content?: string) : React.JSX.Element {
     const blurhash = '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
     const [ flipped, flip ] = useReducer((prev) => !prev, false);
     const duration = 500;
@@ -21,47 +21,48 @@ export function BlogPost( image: string, style?: StyleProp<ImageStyle>, title?: 
     });
 
     const blogPost = 
-        <Animated.View
-            style={[style, flipStyle]}
-            onPointerDown={ flip }
-        >
-            { !flipped &&
-                <ThemedView
-                    lightColor="transparent"
-                    darkColor="transparent"
-                    style={{
-                        alignSelf: "center",
-                        alignItems: "center",
-                    }}
-                >
-                
-                    <Image
-                        id={ id }
-                        style={[style, flipStyle]}
-                        source={image}
-                        placeholder={blurhash}
-                        contentFit="cover"
-                        transition={1000}
-                    />
-                </ThemedView>
-            }
-            { flipped &&
-                <ThemedView
-                    lightColor="transparent"
-                    darkColor="transparent"
-                    style={{
-                        alignSelf: "center",
-                        alignItems: "center",
-                        display: "flex",
-                        flexDirection: "column",
-                        transform: "rotateY(180deg)",
-                    }}
-                >
-                    <ThemedText type="title" lightColor="#37352f">{ title }</ThemedText>
-                    <ThemedText type="subtitle" lightColor="#787774">{ subtitle }</ThemedText>
-                    <ThemedText type="default" lightColor="#787774">{ content }</ThemedText>
-                </ThemedView>
-            }
-        </Animated.View>;
+        <div key={ id } data-grid={ layout }>
+            <Animated.View
+                style={[style, flipStyle]}
+                onPointerDown={ flip }
+            >
+                { !flipped &&
+                    <ThemedView
+                        lightColor="transparent"
+                        darkColor="transparent"
+                        style={{
+                            alignSelf: "center",
+                            alignItems: "center",
+                        }}
+                    >
+                    
+                        <Image
+                            style={[style, flipStyle]}
+                            source={image}
+                            placeholder={blurhash}
+                            contentFit="cover"
+                            transition={1000}
+                        />
+                    </ThemedView>
+                }
+                { flipped &&
+                    <ThemedView
+                        lightColor="transparent"
+                        darkColor="transparent"
+                        style={{
+                            alignSelf: "center",
+                            alignItems: "center",
+                            display: "flex",
+                            flexDirection: "column",
+                            transform: "rotateY(180deg)",
+                        }}
+                    >
+                        <ThemedText type="title" lightColor="#37352f" darkColor="#37352f">{ title }</ThemedText>
+                        <ThemedText type="subtitle" lightColor="#787774" darkColor="#787774">{ subtitle }</ThemedText>
+                        <ThemedText type="default" lightColor="#787774" darkColor="#787774">{ content }</ThemedText>
+                    </ThemedView>
+                }
+            </Animated.View>
+        </div>;
     return blogPost as React.JSX.Element;
 }
