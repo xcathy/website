94310a4f41e3eab626d8baa89e993fa6415bfc4f
lxie <cathyxie97@gmail.com>
2024-11-04 22:27:08 -0800

    updated walking animation

--- components/Cat.tsx
+++ components/Cat.tsx
@@ -1,13 +1,40 @@
 import { Image, ImageStyle } from "expo-image";
 import { Animated, Dimensions, StyleProp, StyleSheet } from "react-native";
 import { Sprites } from '@/constants/Sprites';
+import { useEffect, useReducer, useState } from "react";
 
 export function Cat(e?: PointerEvent, style?: StyleProp<ImageStyle>) : React.JSX.Element {
     const blurhash = '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
+    const spriteDimension = {w: 45, h: 45};
+    const pathLength = Dimensions.get('window').width - spriteDimension.w;
+    const pace = 5;
+    const [ direction, toggleDirection ] = useReducer((prev) => -prev, 1);
+    const [ back, setBack ] = useState<number>(0);
+    const [ forward, setForward ] = useState<number>(0);
     
+    useEffect(() => {
+        setTimeout(() => {
+            if (direction > 0 && (forward + 1) * pace >= pathLength) {
+                toggleDirection();
+            }
+            if (direction < 0 && (back + 1) * pace >= pathLength) {
+                toggleDirection();
+            }
+
+            if (direction > 0) {
+                setBack(0);
+                setForward(forward + 1);
+            }
+            if (direction < 0) {
+                setForward(0);
+                setBack(back + 1);
+            }
+        }, 1000);
+    });
+
     const cat =
         <Animated.View
-            style={ styles().move }
+            style={ styles({direction: direction, forward: forward, back: back, pace: pace}).move }
         >
             <Image
                 style={styles().img}
@@ -15,7 +42,7 @@ export function Cat(e?: PointerEvent, style?: StyleProp<ImageStyle>) : React.JSX
                 placeholder={blurhash}
                 contentFit="fill"
                 transition={1000}
-            />
+            />    
         </Animated.View>;
 
     return cat as React.JSX.Element;
@@ -33,9 +60,10 @@ const styles : any = (props: any) => StyleSheet.create({
         width: 45,
         top: 'auto',
         bottom: 0,
-        left: 'auto',
+        left: props?.direction > 0 ? props?.forward * props?.pace : (Dimensions.get('window').width - 45) - props?.back * props?.pace || 'auto',
         right: 0,
         position: 'absolute',
+        transform: props?.direction > 0 ? 'rotateY(180deg)' : undefined,
         backgroundColor: 'transparent',
     },
 });
