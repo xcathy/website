08e4c2ef0a01c476d1642f90caca9d4da0723d0a
lxie <cathyxie97@gmail.com>
2024-11-04 23:48:58 -0800

    added tooltips

--- components/Cat.tsx
+++ components/Cat.tsx
@@ -1,48 +1,65 @@
 import { Image, ImageStyle } from "expo-image";
-import { Animated, Dimensions, StyleProp, StyleSheet } from "react-native";
+import { Animated, Dimensions, StyleProp, StyleSheet, Text, TouchableHighlight, View } from "react-native";
 import { Sprites } from '@/constants/Sprites';
 import { useEffect, useReducer, useState } from "react";
+import Tooltip from "react-native-walkthrough-tooltip";
 
-export function Cat(e?: PointerEvent, style?: StyleProp<ImageStyle>) : React.JSX.Element {
+export function Cat(style?: StyleProp<ImageStyle>) : React.JSX.Element {
     const blurhash = '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
     const spriteDimension = {w: 45, h: 45};
     const pathLength = Dimensions.get('window').width - spriteDimension.w;
     const pace = 5;
-    const [ direction, toggleDirection ] = useReducer((prev) => -prev, 1);
+    const [ direction, toggleDirection ] = useReducer((prev) => -prev, -1);
     const [ back, setBack ] = useState<number>(0);
     const [ forward, setForward ] = useState<number>(0);
-    
-    useEffect(() => {
-        setTimeout(() => {
-            if (direction > 0 && (forward + 1) * pace >= pathLength) {
-                toggleDirection();
-            }
-            if (direction < 0 && (back + 1) * pace >= pathLength) {
-                toggleDirection();
-            }
+    const [ showTips, setShowTips ] = useState<boolean>(true);
 
-            if (direction > 0) {
-                setBack(0);
-                setForward(forward + 1);
-            }
-            if (direction < 0) {
-                setForward(0);
-                setBack(back + 1);
-            }
-        }, 1000);
+    useEffect(() => {
+        if (!showTips) {
+            setTimeout(() => {
+                if (direction > 0 && (forward + 1) * pace >= pathLength) {
+                    toggleDirection();
+                }
+                if (direction < 0 && (back + 1) * pace >= pathLength) {
+                    toggleDirection();
+                }
+
+                if (direction > 0) {
+                    setBack(0);
+                    setForward(forward + 1);
+                }
+                if (direction < 0) {
+                    setForward(0);
+                    setBack(back + 1);
+                }
+            }, 1000);
+        }  
     });
 
     const cat =
         <Animated.View
+            onPointerUp={() => setShowTips(true)}
             style={ styles({direction: direction, forward: forward, back: back, pace: pace}).move }
         >
-            <Image
-                style={styles().img}
-                source={Sprites.catWalkCycle}
-                placeholder={blurhash}
-                contentFit="fill"
-                transition={1000}
-            />    
+            <Tooltip
+                isVisible={showTips}
+                content={
+                    <Text>{ `Hello!\nYou can press the photos to flip to see the blog posts!\nThanks for visiting my page! :3\n(click anywhere to close this tooltip)` } </Text>
+                }
+                placement="top"
+                onClose={() => setShowTips(false)}
+                tooltipStyle={{ shadowColor: 'transparent' }}
+            >
+                <View style={{ justifyContent: 'flex-start', alignItems: 'center', cursor: 'pointer' }}>
+                    <Image
+                        style={styles().img}
+                        source={Sprites.catWalkCycle}
+                        placeholder={blurhash}
+                        contentFit="fill"
+                        transition={1000}
+                    />
+                </View>
+            </Tooltip>
         </Animated.View>;
 
     return cat as React.JSX.Element;
