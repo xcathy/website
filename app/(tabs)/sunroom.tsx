7ec2a739afd0b9b45f395b9f4f7cc9d3f0cc4e1e
lxie <cathyxie97@gmail.com>
2024-11-12 01:22:15 -0800

    updated background

--- app/(tabs)/sunroom.tsx
+++ app/(tabs)/sunroom.tsx
@@ -11,7 +11,6 @@ import { RadioItem } from '@/components/RadioItem';
 import { PlantItem } from '@/components/PlantItem';
 
 export default function TabTwoScreen() {
-    const blurhash = '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
     const screenW = Dimensions.get('window').width;
     const screenH = Dimensions.get('window').height;
     const clock = document.getElementById("clock");
