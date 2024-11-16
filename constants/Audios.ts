c8399fd13934627228627e3500f257162b504653
lxie <cathyxie97@gmail.com>
2024-11-15 21:41:15 -0800

    using raw permalink

--- constants/Audios.ts
+++ constants/Audios.ts
@@ -1,7 +1,7 @@
 // Radio musics
-const morning = '../assets/audios/music/morning.mp3';
-const raindrops = '../assets/audios/music/raindrops.mp3';
-const happy = '../assets/audios/music/happy.mp3';
+const morning = 'https://github.com/xcathy/website/raw/e9639f9fe70311b34641443ec59eb6200b28224c/assets/audios/music/morning.mp3';
+const raindrops = 'https://github.com/xcathy/website/raw/e9639f9fe70311b34641443ec59eb6200b28224c/assets/audios/music/raindrops.mp3';
+const happy = 'https://github.com/xcathy/website/raw/e9639f9fe70311b34641443ec59eb6200b28224c/assets/audios/music/happy.mp3';
 
 export const Audios = {
     raindrops,
