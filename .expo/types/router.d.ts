3444fb7554f07bbe891bd0c4271811a8cc41b93b
xcathy <cathyxie97@gmail.com>
2025-02-02 10:04:43 -0800

    updated dist folder

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,7 +6,7 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/about` | `/(tabs)/blog` | `/(tabs)/contacts` | `/(tabs)/desk` | `/(tabs)/game` | `/_sitemap` | `/about` | `/blog` | `/contacts` | `/desk` | `/game`;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/about` | `/(tabs)/blog` | `/(tabs)/contacts` | `/(tabs)/desk` | `/(tabs)/game` | `/..\constants\Games` | `/_sitemap` | `/about` | `/blog` | `/contacts` | `/desk` | `/game`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
