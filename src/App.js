9520ee6e96a2e29ba5b8ddc3936fb97556ed5158
xcathy <cathyxie97@gmail.com>
2025-02-06 16:46:28 -0800

    updated mobile tabs

--- src/App.js
+++ src/App.js
@@ -7,19 +7,41 @@ import Contacts from './pages/contacts';
 import Game from './pages/game';
 import './App.css';
 import '../src/styles/global.css';
+import { useState } from 'react';
+import { Specs } from './constants/Specs';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faChevronUp } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+  const mobileView = Specs.screenWidth < 600;
+  const [ displayTab, setDisplayNav ] = useState(!mobileView);
   return (
     <HashRouter>
-      <div className='navigation'>
-        <nav>
-          <Link to="/" className='nunito-medium'>HOME</Link>
-          <Link to="/blog" className='nunito-medium'>BLOG</Link>
-          <Link to="/desk" className='nunito-medium'>DESK</Link>
-          <Link to="/about" className='nunito-medium'>ABOUT</Link>
-          <Link to="/contacts" className='nunito-medium'>CONTACTS</Link>
-        </nav>
-      </div>
+      { displayTab ?
+        <div className='navigation-expanded'>
+          <nav>
+            <Link to="/" className='nunito-medium'>HOME</Link>
+            <Link to="/blog" className='nunito-medium'>BLOG</Link>
+            <Link to="/desk" className='nunito-medium'>DESK</Link>
+            <Link to="/about" className='nunito-medium'>ABOUT</Link>
+            <Link to="/contacts" className='nunito-medium'>CONTACTS</Link>
+            <FontAwesomeIcon
+              icon={faChevronUp}
+              className='collapse-icon'
+              onClick={() => { setDisplayNav(false) }}  
+            />
+
+          </nav>
+        </div>
+        :
+        <div className='navigation-collapsed'>
+          <FontAwesomeIcon
+            icon={faBars}
+            className='navigation-icon'
+            onClick={() => { setDisplayNav(true) }}  
+          />
+        </div>
+      }
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog" element={<Blog />} />
